43efda0eb83f8592792cc2e64a09c6af85f64034
khosravipour-salman <67480607+khosravipour-salman@users.noreply.github.com>
2021-05-16 12:53:25 +0430

    remove secret key

--- blog_prj/settings.py
+++ blog_prj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xok8y)2o(kvj#mj_mk=yqhd6i%pa43#^t^h!u9k6@+xp5g1p%-'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
